d06deed26d374e89d62e58601b38ff510e6cf426
daw202 <larcol01@educantabria.es>
2024-05-09 00:02:39 +0200

    clase conexion con todo el codigo ya hecho y con comentarios

--- conexion.php
+++ conexion.php
@@ -7,11 +7,16 @@
  */
 
 function getConnexion(){
+    // Establece la conexión con la base de datos utilizando mysqli_connect()
     $conexion = mysqli_connect('localhost', 'root', '', "bibliotecauniversitaria");
+    // Verifica si hay errores de conexión utilizando mysqli_connect_errno()
     if (mysqli_connect_errno()) {
+        // Si hay un error, muestra un mensaje de error
         echo "Fallo al conectar a MySQL: " . mysqli_connect_error();
     }
+    // Establece el conjunto de caracteres de la conexión a utf8 para admitir caracteres especiales
     $conexion->set_charset('utf8');
+     // Devuelve la conexión establecida
     return $conexion;
     //
 }
